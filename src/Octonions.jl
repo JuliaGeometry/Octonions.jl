ab0e08f4383f0fa37c78df0938f962ef462aa2d2
Seth Axen <seth@sethaxen.com>
2022-09-19 17:52:32 +0200

    Add and use missing imports/deps

--- src/Octonions.jl
+++ src/Octonions.jl
@@ -1,5 +1,15 @@
 module Octonions
 
+import Base: +, -, *, /, ^, ==
+import Base: abs, abs2, conj, exp, inv, isreal, isfinite, isinf, iszero, isnan, log, real, sqrt
+import Base: promote_rule, float
+import Base: rand, randn
+import LinearAlgebra: normalize
+using Quaternions: Quaternion
+using Random
+
+Base.@irrational INV_SQRT_EIGHT 0.3535533905932737622004 sqrt(big(0.125))
+
 include("octonion.jl")
 
 export Octonion, OctonionF16, OctonionF32, OctonionF64
